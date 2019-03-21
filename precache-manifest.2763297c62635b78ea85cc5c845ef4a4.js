c76b92f5a2bf6c2cbdec78e97c0198bb91643b82
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-03-21 15:47:43 +0200

    deploy

---
+++ precache-manifest.2763297c62635b78ea85cc5c845ef4a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e59b3ac907386fd04f4f3ad39813cfc2",
+    "url": "/voting-jp/static/media/logo-white.e59b3ac9.svg"
+  },
+  {
+    "revision": "d8c1c882cd1a7d6d889b",
+    "url": "/voting-jp/static/js/runtime~main.d8c1c882.js"
+  },
+  {
+    "revision": "3a9f07fae4c2d853327c",
+    "url": "/voting-jp/static/js/main.3a9f07fa.chunk.js"
+  },
+  {
+    "revision": "e0005240df61f2bb6a47",
+    "url": "/voting-jp/static/js/2.e0005240.chunk.js"
+  },
+  {
+    "revision": "3a9f07fae4c2d853327c",
+    "url": "/voting-jp/static/css/main.e5d6e8ab.chunk.css"
+  },
+  {
+    "revision": "4289c81a19578ba17e09cb2cdd778212",
+    "url": "/voting-jp/index.html"
+  }
+];
