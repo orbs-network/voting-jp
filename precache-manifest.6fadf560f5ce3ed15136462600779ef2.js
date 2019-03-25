90ff997fad3b1e54237b7f996f71d5f1d8b99739
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-03-25 11:43:23 +0200

    deploy

---
+++ precache-manifest.6fadf560f5ce3ed15136462600779ef2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e59b3ac907386fd04f4f3ad39813cfc2",
+    "url": "/voting-jp/static/media/logo-white.e59b3ac9.svg"
+  },
+  {
+    "revision": "d8c1c882cd1a7d6d889b",
+    "url": "/voting-jp/static/js/runtime~main.d8c1c882.js"
+  },
+  {
+    "revision": "138c143bb1b5b31dae01",
+    "url": "/voting-jp/static/js/main.138c143b.chunk.js"
+  },
+  {
+    "revision": "65e490f87ce676bcb294",
+    "url": "/voting-jp/static/js/2.65e490f8.chunk.js"
+  },
+  {
+    "revision": "138c143bb1b5b31dae01",
+    "url": "/voting-jp/static/css/main.e5d6e8ab.chunk.css"
+  },
+  {
+    "revision": "c024ba7f69f9fa6e8e9465901fb95bb7",
+    "url": "/voting-jp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/voting-jp/precache-manifest.2617374867d83be6e6c45e518bf06e62.js"
+  "/voting-jp/precache-manifest.6fadf560f5ce3ed15136462600779ef2.js"
 );
 
 workbox.clientsClaim();
